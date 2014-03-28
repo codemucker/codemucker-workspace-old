ca7c13ba3e6fe7da726d065533efcc4b726328ee
Bert van Brakel <bertvanbrakel+testfirst@gmail.com>
2014-03-28 16:39:46 +1300

    script update

--- bin/commit_all.sh
+++ bin/commit_all.sh
@@ -4,8 +4,11 @@ read msg
 for  d in codemucker-*; 
 	do if [ -d $d ]; then 
 		pushd $d > /dev/null; 
-		echo "======== $d"; 
-		git add .; 
+		echo "======== $d";
+		git add . --all; 
+		git status --short;
+		echo -n "commit?"
+		read answer;
 		git commit -m "$msg"; 
 		popd >/dev/null; 
 	fi; 
