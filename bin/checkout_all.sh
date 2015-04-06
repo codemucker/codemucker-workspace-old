6b6a0ff684274824b6f610e041faede80141df27
Bert van Brakel <bertvanbrakel@gmail.com>
2015-04-06 09:45:46 +0930

    update checkout script

--- bin/checkout_all.sh
+++ bin/checkout_all.sh
@@ -5,12 +5,13 @@ git clone git@github.com:codemucker/codemucker-jpattern.git
 git clone git@github.com:codemucker/codemucker-jfind.git
 git clone git@github.com:codemucker/codemucker-jmutate.git
 git clone git@github.com:codemucker/codemucker-jtest.git
-git clone git@github.com:codemucker/codemucker-orientdb.git
+#git clone git@github.com:codemucker/codemucker-orientdb.git
 git clone git@github.com:codemucker/codemucker-testfirst.git
 git clone git@github.com:codemucker/codemucker-testserver.git
-git clone git@github.com:codemucker/codemucker-cqrs.git
+#git clone git@github.com:codemucker/codemucker-cqrs.git
 git clone git@github.com:codemucker/jmutate-maven-plugin.git
-git clone git@github.com:codemucker/codemucker-infrastructure.git
+#git clone git@github.com:codemucker/codemucker-infrastructure.git
+git clone git@github.com:codemucker/codemucker-csv.git
 
 #git clone git@github.com:codemucker/root.git
 #git clone git@github.com:TestFirstNetNet/TestFirst.Net.git
