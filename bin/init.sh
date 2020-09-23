780669811743566f0ac5f129bf8bb6a2c073b26e
Bert van Brakel <bertvanbrakel@gmail.com>
2020-09-23 13:20:27 +1000

    rename projects.json to tk.projects.json

--- bin/init.sh
+++ bin/init.sh
@@ -8,8 +8,14 @@ set -Eeuo pipefail & set -e
 
 cd "${0%/*}/.." # set the CWD to the root dir
 
-git clone git@github.com:codemucker/tk.git tools/tk
-pushd ./tools/tk >/dev/null
-    git checkout master
-    bin/tk projects clone
-popd >/dev/null
+if ! command -v tk command -v o &> /dev/null; then
+    echo "'tk' is not installed'" 
+    git clone git@github.com:codemucker/tk.git tools/tk
+    pushd ./tools/tk >/dev/null
+        git checkout master
+        bin/tk projects clone
+    popd >/dev/null
+else
+    tk projects clone
+fi
+
